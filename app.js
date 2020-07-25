2b92553482b5998735d2d744d615680be2ead195
Mcnikko Pinlac <mcnikkopinlacnetflix@gmail.com>
2020-07-26 01:18:56 +0800

    salting and hashing using bcrypt

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //jshint esversion:6
  require('dotenv').config()
+ // const md5 = require('md5');
  const express = require('express');
  const bodyParser = require('body-parser');
  const ejs = require('ejs');
  const mongoose = require('mongoose');
- const encrypt = require('mongoose-encryption');
+ // const encrypt = require('mongoose-encryption');
+ const bcrypt = require('bcrypt');
+ const saltRounds = 10;
  const app = express();
 
  app.use(express.static('public'));
@@ -19,7 +22,7 @@
  });
 
 
- userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ['password']});
+ // userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ['password']});
 
  const User = new mongoose.model('User',userSchema);
 
@@ -36,17 +39,21 @@ app.get('/register', (req,res) => {
 });
 
 app.post('/register', function(req,res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
 
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    } else {
-      res.render('secrets');
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      // Store hash in your password DB.
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+      });
+
+      newUser.save(function(err){
+        if(err){
+          console.log(err);
+        } else {
+          res.render('secrets');
+        }
+      });
   });
 });
 
@@ -54,17 +61,24 @@ app.post('/login', function(req,res){
   const username = req.body.username;
   const password = req.body.password;
 
-  User.findOne({email: username}, function(err,foundUser){
-      if (err) {
-        console.log(err);
-      } else {
-        if (foundUser) {
-          if (foundUser.password === password) {
-            res.render('secrets');
+    User.findOne({email: username}, function(err,foundUser){
+        if (err) {
+          console.log(err);
+        } else {
+          if (foundUser) {
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+              if (result === true) {
+                res.render('secrets');
+              } else {
+                console.log('Invalid Password!');
+              }
+            });
           }
         }
-      }
-  });
+    });
+
+
+
 });
 
  app.listen(3000, () => {
